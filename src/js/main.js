77f9c3aaff272f3e680cb22c672b88b519b97826
camila0424 <milabs.esp24@gmail.com>
2025-05-20 11:58:15 +0200

    fetch, animes pintados en pag y estilos de tarjetas

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,65 @@
 'use strict';
 
+const animeUl  = document.querySelector(".js_animeUl ");
+const filterByNameInput =document.querySelector(".js_filterByNameInput")
+const filterButton = document.querySelector(".js_filterButton")
+const selectButton =document.querySelector (".js_selectButton")
+/* const favouritesUl = document.querySelector (".js_favouritesUl") */
 
+// DATOS
+
+let allMovies =[];
+// FUNCIONES
+
+function renderOneMovie(oneMovie) {    //render de 1 movie
+    const html = `
+    <li class="anime-item">
+        <h2 class="name">${oneMovie.title}</h2>          
+        <img src="${oneMovie.images.jpg.image_url}" alt="">
+        <input class="selectbtn js_selectButton"
+            type="submit"
+            value="Agregar a favoritos"
+        />
+    </li>
+  `;
+
+  animeUl.innerHTML += html;
+
+  return html;
+}
+
+function renderAllMovies() {
+  let html = '';
+    console.log ('antes del all movies', allMovies)
+  for( const oneMovie of allMovies) {
+    html += renderOneMovie(oneMovie);
+  }
+
+  animeUl.innerHTML = html;
+}
+
+
+// CUANDO CARGA LA PÁGINA
+
+fetch('https://api.jikan.moe/v4/anime')
+  .then(res => res.json())
+  .then(data => {
+    allMovies = data.data;
+    console.log('despues del data',data)
+    renderAllMovies();
+});
+
+
+/* renderOneMovie(oneMovie); */
+
+filterByNameInput.addEventListener("click", (event) =>{
+    event.preventDefault()
+})
+
+filterButton.addEventListener("click", (event) =>{
+    event.preventDefault()
+})
+
+selectButton.addEventListener("click", (event) =>{
+    event.preventDefault()
+})
