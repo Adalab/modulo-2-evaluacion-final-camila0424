4c2f29483eced2ce6a11477c5d8820ff1f4e2e5f
camila0424 <milabs.esp24@gmail.com>
2025-05-21 10:41:12 +0200

    final de evaluación requerida con algunos bonus. Página funcional

--- src/js/main.js
+++ src/js/main.js
@@ -5,40 +5,42 @@ const filterByNameInput =document.querySelector(".js_filterByNameInput")
 const filterButton = document.querySelector(".js_filterButton")
 const favouritesUl = document.querySelector (".js_favouritesUl")
 const resetButton = document.querySelector('.js_resetButton')
-// DATOS
+
+
+//---------------------------------------------- DATOS----------------------------------------------------------
 
 let allMovies =[];
 let favourites = [];
 
-// FUNCIONES
+// -------------------------------------------FUNCIONES-------------------------------------------------------------
 
-function renderOneMovie(oneMovie) {    //render de 1 movie
+function renderOneMovie(oneMovie) {    //render de 1 movie. Se pone el Li que se va a mostrar en el html
     const html = `
     <li class="anime-item js_animeItemLi" data-gancho="${oneMovie.mal_id}">
         <h2 class="name">${oneMovie.title}</h2>          
         <img src="${oneMovie.images.jpg.image_url}" alt="">
     </li> `;
 
-  animeUl.innerHTML += html;
-
   return html;
 }
 
+
+
 function renderAllMovies() {
   let html = '';
-    console.log ('antes del all movies', allMovies)
-
+   
   for( const oneMovie of allMovies) {
     html += renderOneMovie(oneMovie);
   }
 
-  animeUl.innerHTML += html;
+  animeUl.innerHTML = html;
 
   const allAnimeItemLi = document.querySelectorAll(".js_animeItemLi");
   for(const animeItemLi of allAnimeItemLi){
     animeItemLi.addEventListener('click', hadleClickMovies)
   }
 }
+
 
 
 function renderAllFavourites() {
@@ -52,70 +54,59 @@ function renderAllFavourites() {
 }
 
 
+
 function hadleClickMovies(ev){
+ 
     const clickedLi = ev.currentTarget;
     clickedLi.classList.toggle('favourite')
-   // Identificamos el objeto asociado al <li> en el que se ha hecho click.
+   
+    // Identificamos el objeto asociado al <li> en el que se ha hecho click.
     const id_gancho = parseInt( clickedLi.dataset.gancho );
 
-    console.log("gancho de las movies", id_gancho);
-
     // Averiguamos si ya es favorita
-    const moviePositionFromFavs = favourites.findIndex(oneMovie => oneMovie.id === id_gancho);
-    
-    console.log(moviePositionFromFavs);
-    
-    console.log(id_gancho, allMovies[0].mal_id);
-   
-
+    const moviePositionFromFavs = favourites.findIndex(oneMovie => oneMovie.mal_id === id_gancho);
     if( moviePositionFromFavs === -1 ) {
-    // La nave clickada no está en favs  
+      // La peli clickada no está en favoritas  
       const clickedMoviesItem = allMovies.find(oneMovie => oneMovie.mal_id === id_gancho);
-      console.log(clickedMoviesItem);
-
-    // Añadir el objeto en el array de fav
+    
+    
+      // Añadir el objeto en el array de favoritas con push cuando 
       favourites.push(clickedMoviesItem);
 
       localStorage.setItem( 'favouriteMovie', JSON.  stringify(favourites) );
 
-      console.log('allMovies', allMovies);
-      console.log('favourites', favourites);
-
-
-
-
-      // Generamos otro <li> para esos datos
+    // Generamos otro <li> para esos datos
       const htmlOneMovie = renderOneMovie(clickedMoviesItem)
 
-      // Ponemos el <li> en la sección de favoritos
+     // Ponemos el <li> en la sección de favoritos
       favouritesUl.innerHTML += htmlOneMovie;
     }
     else {
-    // La peli clickada SÍ está en favs
-    console.log('La nave YA está en favs');    
-  }
-
+    // La peli clickada SÍ está en favs no hacer nada
+     }
 }
 
-// CUANDO CARGA LA PÁGINA
 
-fetch('https://api.jikan.moe/v4/anime')
-  .then(res => res.json())
-  .then(data => {
-    allMovies = data.data;
-    console.log('despues del data',data)
-    renderAllMovies();
-});
 
-filterByNameInput.addEventListener("click", (event) =>{
-    event.preventDefault()
-})
+// filtrando con el API teniendo en cuenta el título
+function handleClickFilterButton(ev) {
+  ev.preventDefault();
 
-filterButton.addEventListener("click", (event) =>{
-    event.preventDefault()
-})
+  const title = filterByNameInput.value;
 
-    
+  fetch(`https://api.jikan.moe/v4/anime?q=${title}`)
+    .then(res => res.json())
+    .then(data => {
+      allMovies = data.data;
+
+      //quiero que solo me aparezca la que busco entonces vacío la lista. El API me muestra las conincidencias por nombre de peli pero también por personajes
+      animeUl.innerHTML = '';
+
+      renderAllMovies();
+    });
+}
+
+//------------------------------------------------EVENTOS--------------------------------------------------------
 
 resetButton.addEventListener('click', (event) => {
   event.preventDefault();
@@ -125,34 +116,23 @@ resetButton.addEventListener('click', (event) => {
   localStorage.removeItem('favouriteMovie');
 }) 
 
+filterButton.addEventListener('click', handleClickFilterButton);
+
+// -----------------------------------------CUANDO CARGA LA PÁGINA-----------------------------------------------
+
+//el fetch nos trae todas las peliculas del api
+fetch('https://api.jikan.moe/v4/anime')
+  .then(res => res.json())
+  .then(data => {
+    allMovies = data.data;
+   
+    renderAllMovies();
+});
+
 // Obtenemos las favoritos desde el LocalStorage
 const favsFromLS = JSON.parse( localStorage.getItem('favouriteMovie') );
-console.log(favsFromLS);
 
 if( favsFromLS !== null ) {
   favourites = favsFromLS;
   renderAllFavourites();
 }
-
-
-// Filtro con API
-
-function handleClickFilterButton(ev) {
-  ev.preventDefault();
-
-  const title = filterByNameInput.value;
-  //quiero que me filtre por el título puesto en el input pero no funciona con la api en este orden. Intenté ponerle la barra pero dice que es not found
-  fetch(`https://api.jikan.moe/v4/anime/${title}`)
-    .then(res => res.json())
-    .then(data => {
-      allMovies = data.data.title;
-
-      renderAllMovies();
-    });
-}
-
-filterButton.addEventListener('click', handleClickFilterButton);
-
-
-
-//no me agrega nuevas pelis 
